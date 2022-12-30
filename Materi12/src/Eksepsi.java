701cbc118973a8706a20c46836e613b5d2dd7168
AryaSuryaa <suryasaputra914@gmail.com>
2022-12-30 14:21:53 +0800

    Menambahkan eksepsi materi 12

---
+++ Materi12/src/Eksepsi.java
@@ -0,0 +1,17 @@
+public class Eksepsi {
+    public void cetak() {
+        try {
+            String[] kota = {"Jakarta" ,"Bandung" ,"Demak"};
+
+            for(int i = 0; i <= 5; i++) {
+                System.out.println(kota[i] + " ");
+            }
+        } catch (Exception e) {
+            System.out.println("Kesalahan : " + e.getMessage() );
+        }
+    }
+    public static void main(String[] args) {
+        Eksepsi eks = new Eksepsi();
+        eks.cetak();
+    }
+}
