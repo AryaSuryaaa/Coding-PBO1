46b373556e2876c5f4d2ef554033ff2743dcffdc
AryaSuryaa <suryasaputra914@gmail.com>
2023-01-25 09:07:11 +0700

    Jawaban UAS PBO no 1 dan 2

---
+++ UAS/UAS/src/jaawaban1/jawab1.java
@@ -0,0 +1,45 @@
+package jaawaban1;
+
+import java.util.Scanner;
+
+public class jawab1 {
+    public static void main(String[] args) {
+        int x, y, z, terbesar, terkecil;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Menentukan Bilangan Terbesar, Terkecil dan Rata-rata");
+        System.out.print("Masukkan Bilangan Pertama (x) : ");
+        x = input.nextInt();
+        System.out.print("Masukkan Bilangan Kedua (y) : ");
+        y = input.nextInt();
+        System.out.print("Masukkan Bilangan Ketiga (z) : ");
+        z = input.nextInt();
+
+        float jumlah = x + y + z;
+        float rata = jumlah / 3;
+
+        System.out.println("Hasil");
+        System.out.printf("Rata-ratanya adalah : %.15f\n", rata);
+
+        if(x > y && x > z) {
+            terbesar = x;
+        } else if (y > z && y > x) {
+            terbesar = y;
+        } else {
+            terbesar = z;
+        }
+
+        System.out.println(terbesar + " adalah bilangan terbesar");
+
+        if (x < y && x < z) {
+            terkecil = x;
+        } else if (y < x && y < z) {
+            terkecil = y;
+        } else {
+            terkecil = z;
+        }
+
+        System.out.println(terkecil + " adalah bilangan terkecil");
+    }
+}
---
+++ UAS/UAS/src/jawaban2/jawab2.java
@@ -0,0 +1,29 @@
+package jawaban2;
+
+import java.util.Scanner;
+
+public class jawab2 {
+    public static void main(String[] args) {
+        int result = 0, harga;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah barang : ");
+        int banyak = input.nextInt();
+
+        for(int i = 1; i <= banyak; i++) {
+            System.out.print("Masukkan harga barang ke-" + i + " : ");
+            harga = input.nextInt();
+            result += harga;
+        }
+
+        System.out.println("\nTotal belanja adalah " + result);
+
+        if(result >= 1500000) {
+            System.out.println("Mendapat bonus Setrika");
+        } else if (result >= 1000000 && result < 1500000) {
+            System.out.println("Mendapat bonus Payung");
+        } else if (result >= 500000 || result < 1000000) {
+            System.out.println("Mendapat bonus Ballpoint");
+        }
+    }
+}
